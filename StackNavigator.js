d3968cdb1840f94d96ed242a6e1c6579fed9f429
paul-madut <paul.madut@mths.ca>
2023-05-31 23:41:56 -0400

    Fixed structure of app

---
+++ StackNavigator.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Diabetes from "./Screens/Diabetes";
+import Fitness from "./Screens/Fitness";
+import Food from "./Screens/Food";
+import WelcomeWindow from "./Windows/WelcomeWindow";
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  const user = false;
+  return (
+    <Stack.Navigator>
+      {user ? (
+        <>
+          <Stack.Screen name="Diabetes" component={Diabetes} />
+          <Stack.Screen name="Fitness" component={Fitness} />
+          <Stack.Screen name="Food" component={Food} />
+        </>
+      ) : (
+        <Stack.Screen name="WelcomeWindow" component={WelcomeWindow} />
+      )}
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
