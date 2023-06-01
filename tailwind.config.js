5f4f2991b12b849f2307e7ed88840a716921d733
paul-madut <paul.madut@mths.ca>
2023-05-31 23:02:29 -0400

    Tailwind settings

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./App.js", "./Components/**/*.js", "./Screens/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: require("tailwind-rn/unsupported-core-plugins"),
+};
