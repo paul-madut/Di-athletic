c1c3b8697ecde1fd1601fb003a267f12ed218dff
paul-madut <paul.madut@mths.ca>
2023-06-03 20:11:42 -0400

    added colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ["./App.js", "./Components/**/*.js", "./Screens/**/*.js"],
   theme: {
+    colors: {
+      b: "#0D1321",
+      lb: "#748CAB",
+      db: "#1D2D44",
+      bge: "#F0EBD8",
+      bg: "#3E5C76",
     extend: {},
   },
   plugins: [],
